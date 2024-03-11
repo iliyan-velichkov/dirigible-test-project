5c0ed374c34917eac523f9cf7e9bc2a168323909
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-03-11 08:33:38 +0200

    comment throwing an error in the handler

--- dirigible-test-project/listeners/background-handler.js
+++ dirigible-test-project/listeners/background-handler.js
@@ -1,8 +1,8 @@
 
 exports.onMessage = function (message) {
     console.log(new Date() + "### BACKGROUND HANDLER ### - Received a MESSAGE: [" + message + "]");
-    console.log("Throwing an error...")
-    throw new Error("Intentional error");
+    // console.log("Throwing an error...")
+    // throw new Error("Intentional error");
 }
 
 exports.onError = function (error) {
