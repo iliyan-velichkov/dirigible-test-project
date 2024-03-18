720ec86812057a439c334b405b48390d6752ee1f
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-03-18 12:41:16 +0200

    update handler

--- dirigible-test-project/jobs/job-handler.ts
+++ dirigible-test-project/jobs/job-handler.ts
@@ -1,9 +1,5 @@
 import { BookRepository } from "../gen/dao/Books/BookRepository";
 
-console.log("Executing testing Job...");
-
 const repo = new BookRepository();
 const books = repo.findAll();
 console.log(`Found [${books.length}] books. Books: [${JSON.stringify(books)}]`);
-
-console.log("Testing Job completed.");
