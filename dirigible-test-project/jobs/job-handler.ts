aa435d23227b3956ccf8b248e501bcac53f65038
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-03-18 14:53:56 +0200

    use logger

--- dirigible-test-project/jobs/job-handler.ts
+++
@@ -1,5 +0,0 @@
-import { BookRepository } from "../gen/dao/Books/BookRepository";
-
-const repo = new BookRepository();
-const books = repo.findAll();
-console.log(`Found [${books.length}] books. Books: [${JSON.stringify(books)}]`);
